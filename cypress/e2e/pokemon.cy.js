f03917e876c593d1aa1d9b6ce069b36c137139e6
文静 <ruhuiwen0608@gmail.com>
2023-08-11 15:00:21 +0300

    add second e2e test: pokemon page can be navigated to

--- cypress/e2e/pokemon.cy.js
+++ cypress/e2e/pokemon.cy.js
@@ -5,4 +5,9 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+  it('pokemon page can be navigated to', function() {
+    cy.visit('http://localhost:8080/')
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
+  })
 })
